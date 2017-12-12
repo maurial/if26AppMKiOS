98fa0b26f2fc59e0935a2bc5266f1f4c6a7cf8d9
if26-grp3 <if26-grp3@mac04-m102.utt.fr>
2017-12-12 21:11:26 +0100

    marial

--- if26AppMK/ViewController.swift
+++ if26AppMK/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SQLite3
 
 class ViewController: UIViewController {
 
---
+++ if26AppMKDatabaseHeader.h
@@ -0,0 +1,13 @@
+//
+//  if26AppMKDatabaseHeader.h
+//  if26AppMK
+//
+//  Created by if26-grp3 on 12/12/2017.
+//  Copyright © 2017 if26-grp3. All rights reserved.
+//
+
+#ifndef if26AppMKDatabaseHeader_h
+#define if26AppMKDatabaseHeader_h
+
+
+#endif /* if26AppMKDatabaseHeader_h */
