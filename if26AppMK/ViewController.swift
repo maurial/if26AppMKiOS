4aed634ffd5d95685c0f0ca3e2150942a11d1763
if26-grp3 <if26-grp3@mac04-m102.utt.fr>
2017-12-12 16:52:35 +0100

    Initial Commit

---
+++ if26AppMK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  if26AppMK
+//
+//  Created by if26-grp3 on 12/12/2017.
+//  Copyright © 2017 if26-grp3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
